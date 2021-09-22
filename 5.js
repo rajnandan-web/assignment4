3f9f8cab70758d532d3f69c4b0c61e14eaf8b152
Rajnandan Rajbanshi <83878581+rajnandan-web@users.noreply.github.com>
2021-09-22 14:50:34 +0530

    Add files via upload

---
+++ 5.js
@@ -0,0 +1,12 @@
+
+
+const user = { person: 
+    { employee: { firstname: "Rahul" }, 
+    likes: ["Cycling", "Hiking", "Driving"] } }
+const name="Rahul" 
+const like1="cycling"
+const like2="Hiking"
+const like3="Driving"
+ 
+console.log(user.person.employee.firstname) ;
+console.log(like2)  ;
